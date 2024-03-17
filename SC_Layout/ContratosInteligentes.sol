30319145451295fc0cb38b48a752779be53be970
Barba <patrickamc@protonmail.com>
2024-03-17 14:13:47 -0300

    Contract Layout

---
+++ SC_Layout/ContratosInteligentes.sol
@@ -0,0 +1,39 @@
+//SPX-License-Identifier: INSIRA A LICENÇA AQUI
+
+///Pragma Version///
+
+///Imports///
+
+///Errors///
+
+///Interfaces, Libraries///
+
+contract ContractName{
+
+    ///Type declarations///
+
+    ///State variables///
+
+    ///Events///
+
+    ///Modifiers///
+
+    ///Functions///
+
+    /* Layout of Functions: */
+    ///constructor///
+
+    ///receive function (if exists)///
+
+    ///fallback function (if exists)///
+
+    ///external///
+
+    ///public///
+
+    ///internal///
+
+    ///private///
+
+    ///view & pure functions///
+}
